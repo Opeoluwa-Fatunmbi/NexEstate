f4b9c04fdeb4e9dd16304d9a6e8b0aa42740fb81
Opeoluwa Fatunmbi <opeoluwafatunmbi@gmail.com>
2023-12-23 00:48:53 +0100

    [Fix] Makefile

--- nexestate/settings/development.py
+++ nexestate/settings/development.py
@@ -1,3 +1,19 @@
 from .base import *
 
 DEBUG = True
+
+
+
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("POSTGRES_DB"),
+        "USER": config("POSTGRES_USER"),
+        "PASSWORD": config("POSTGRES_PASSWORD"),
+        "HOST": config("POSTGRES_SERVER"),
+        "PORT": config("POSTGRES_PORT"),
+    }
+}
+
--- nexestate/settings/production.py
+++ nexestate/settings/production.py
@@ -1,3 +1,15 @@
 from .base import *
 
 DEBUG = False
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("POSTGRES_DB"),
+        "USER": config("POSTGRES_USER"),
+        "PASSWORD": config("POSTGRES_PASSWORD"),
+        "HOST": config("POSTGRES_SERVER"),
+        "PORT": config("POSTGRES_PORT"),
+    }
+}
