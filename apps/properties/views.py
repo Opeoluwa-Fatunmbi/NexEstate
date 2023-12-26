fff96dc2c4b21172cca8a1c5feb2ef9908c6443c
Opeoluwa Fatunmbi <opeoluwafatunmbi@gmail.com>
2023-12-26 17:41:45 +0100

    [Fix] Property tax

--- apps/properties/views.py
+++ apps/properties/views.py
@@ -330,7 +330,9 @@ class PropertyTaxAPIView(APIView):
         try:
             property = Property.objects.get(slug=slug)
 
-            tax = property.price * property.tax
+
+            # tax = property.price * property.tax
+            tax = Property.final_property_price(property)
             return CustomResponse.success(tax)
         except:
             return CustomResponse.error(
