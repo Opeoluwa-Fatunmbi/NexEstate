379f4b4ee99021f7698864333c1b5b4969167dd4
Opeoluwa Fatunmbi <opeoluwafatunmbi@gmail.com>
2023-12-28 16:18:43 +0100

    [Fix] Default Settinggs

--- nexestate/settings/base.py
+++ nexestate/settings/base.py
@@ -9,7 +9,10 @@ from django.utils.log import DEFAULT_LOGGING
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
-SETTINGS = config("SETTINGS")
+
+
+SETTINGS = config("SETTINGS", default="default_value")
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config("SECRET_KEY")
