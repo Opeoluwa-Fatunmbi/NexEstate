bebee3bd900b9d1116595ed31ff12f2a846e16ad
Opeoluwa Fatunmbi <opeoluwafatunmbi@gmail.com>
2023-12-30 21:15:45 +0100

    [Deployment Config]

--- nexestate/settings/base.py
+++ nexestate/settings/base.py
@@ -161,11 +161,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static/")]
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "static/media/")
+
 
 
 # Cloudinary settings
