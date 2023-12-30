09e244996d9d2be14dfdd53d4c5fa6a2d998de8e
Opeoluwa Fatunmbi <opeoluwafatunmbi@gmail.com>
2023-12-30 20:28:20 +0100

    [Deployment]

--- build_files.sh
+++
@@ -1,5 +0,0 @@
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py migrate
-python3.9 manage.py collectstatic --noinput --clear
-python3.9 manage.py python-decouple
-python3.9 manage.py decouple
