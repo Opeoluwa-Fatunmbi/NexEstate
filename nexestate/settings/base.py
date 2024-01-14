d7355e39deb0256970b7a56da0abb3bf23106ef5
Opeoluwa Fatunmbi <opeoluwafatunmbi@gmail.com>
2024-01-14 05:29:32 +0100

    [Fix] Setting Optimization

--- nexestate/settings/base.py
+++ nexestate/settings/base.py
@@ -167,6 +167,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static/")]
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "static/media/")
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+
 
 
 # Cloudinary settings
