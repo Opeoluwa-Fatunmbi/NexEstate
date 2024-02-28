0f77791aac5f9ee289700d9d7630ea29219576fe
Opeoluwa Fatunmbi <opeoluwafatunmbi@gmail.com>
2024-02-28 17:08:33 +0100

    [Add] Docker Compose

--- nexestate/settings/development.py
+++ nexestate/settings/development.py
@@ -1,18 +1,20 @@
 from .base import *
-import dj_database_url
+
+# import dj_database_url
+from decouple import config
 
 DEBUG = True
 
-#
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": config("POSTGRES_DB"),
-#         "USER": config("POSTGRES_USER"),
-#         "PASSWORD": config("POSTGRES_PASSWORD"),
-#         "HOST": config("POSTGRES_HOST"),
-#         "PORT": config("POSTGRES_PORT"),
-#     }
-# }
 
-DATABASES = {"default": dj_database_url.config(default=config("DATABASE_URL"))}
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("POSTGRES_DB"),
+        "USER": config("POSTGRES_USER"),
+        "PASSWORD": config("POSTGRES_PASSWORD"),
+        "HOST": config("POSTGRES_HOST"),
+        "PORT": config("POSTGRES_PORT"),
+    }
+}
+
+# DATABASES = {"default": dj_database_url.config(default=config("DATABASE_URL"))}
